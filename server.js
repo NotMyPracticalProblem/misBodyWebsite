fe29c51270ede36f4cedf2a1e7d41693210ed3f7
Alex Hammett <alexhammett@misstudent.com>
2019-09-06 10:18:28 -0400

    Added "/" dir for index.html
    Added the $GET request for localhost:3000 to direct to index.html as well as localhost:3000/home

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 var path = require('path');
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 app.get('/home', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/about.html'));
+    res.sendFile(path.join(__dirname + '/about.html'))
   });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
