1dd94ad10862c27f2a13710688cc4c8be63f583a
Alex Hammett <alexhammett@misstudent.com>
2019-10-04 10:53:19 -0400

    Redid way more shiz and added footer

--- nav.js
+++ nav.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    function createNav() {
-      var navTitles = ['index', 'about', 'articles', 'signup', 'government'];
-      for (var i=0; i < navTitles.length; i++) {
-          $("#navLinks").append("<li><a href='" + navTitles[i] + "'.html'>" + navTitles[i] + "</a></li>");
-      }
+  function createNav() {
+    var navTitles = ['home', 'about', 'articles', 'signup', 'stdgvt'];
+    for (var i = 0; i < navTitles.length; i++) {
+      $("#navLinks").append("<li><a href='" + navTitles[i] + "'.html'>" + navTitles[i] + "</a></li>");
     }
-    createNav();
+  }
+  createNav();
 
 
-  });
-  
+});
